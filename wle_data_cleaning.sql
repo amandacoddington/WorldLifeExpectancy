4081c4b20a7c1fef724c51b1f33e891013187ab1
amandacoddington <akcoddington64@gmail.com>
2024-10-17 15:14:33 -0400

    adding to script

--- wle_data_cleaning.sql
+++ wle_data_cleaning.sql
@@ -24,7 +24,8 @@ SELECT Row_ID,
 FROM world_life_expectancy;
 
 
--- filtering the results of the above query for row numbers that are greater than 1 which gives the row_id for the duplicates
+-- filtering the results of the above query for row numbers that are greater 
+-- than 1 which gives the row_id for the duplicates
 SELECT *
 FROM (
         SELECT Row_ID,
@@ -62,7 +63,8 @@ FROM world_life_expectancy
 WHERE Status = '';
 
 
--- checking to ensure each country has at least one status column to pull from & determining what values are used in the status column
+-- checking to ensure each country has at least one status column to pull from 
+-- & determining what values are used in the status column
 SELECT DISTINCT(Status)
 FROM world_life_expectancy
 WHERE Status <> '';
@@ -74,10 +76,12 @@ FROM world_life_expectancy
 WHERE Status = 'Developing';
 
 
--- updating the table & populating 'Developing' in blank rows under status for countries that were identified in previous query 
+-- updating the table & populating 'Developing' in blank rows under status for
+-- countries that were identified in previous query 
 -- by updating the table, joining the table to itself in the update statement
 UPDATE world_life_expectancy t1
-    JOIN world_life_expectancy t2 ON t1.Country = t2.Country
+    JOIN world_life_expectancy t2 
+    ON t1.Country = t2.Country
 SET t1.Status = 'Developing'
 WHERE t1.Status = ''
     AND t2.Status <> ''
@@ -86,7 +90,8 @@ WHERE t1.Status = ''
 
 -- same process for the 'Developed' status countries
 UPDATE world_life_expectancy t1
-    JOIN world_life_expectancy t2 ON t1.Country = t2.Country
+    JOIN world_life_expectancy t2 
+    ON t1.Country = t2.Country
 SET t1.Status = 'Developed'
 WHERE t1.Status = ''
     AND t2.Status <> ''
@@ -110,4 +115,44 @@ SELECT Country,
 FROM world_life_expectancy;
 
 
--- if a country's life expectancy is blank for a given year, using the average of the previous year and next year's life expectancy to populate life expectancy for the blank year
+-- if a country's life expectancy is blank for a given year, will use the 
+-- average of the previous year and next year's life expectancy to populate life 
+-- expectancy for the blank year
+SELECT t1.Country, t1.Year, t1.`Life expectancy`,
+    t2.Country, t2.Year, t2.`Life expectancy`,
+    t3.Country, t3.Year, t3.`Life expectancy`,
+    ROUND((t2.`Life expectancy` + t3.`Life expectancy`)/2,1)
+FROM world_life_expectancy t1
+JOIN world_life_expectancy t2
+    ON t1.Country = t2.Country
+    AND t1.Year = t2.Year - 1
+JOIN world_life_expectancy t3
+    ON t1.Country = t3.Country
+    AND t1.Year = t3.Year + 1   
+WHERE t1.`Life expectancy` = ''
+;
+
+
+-- updating the table to include the populated life expectancy 
+UPDATE world_life_expectancy t1
+JOIN world_life_expectancy t2 
+	ON t1.Country = t2.Country
+    AND t1.Year = t2.Year - 1 
+JOIN world_life_expectancy t3 
+	ON t1.Country = t3.Country
+    AND t1.Year = t3.Year + 1
+SET  t1.`Life expectancy` = ROUND((t2.`Life expectancy` + t3.`Life expectancy`)/2,1)
+WHERE  t1.`Life expectancy` = ''
+;
+
+
+-- double checking it populated the life expectancy correctly and ensuring there
+-- is no more blanks
+SELECT Country, Year, `Life expectancy`
+FROM world_life_expectancy
+;
+
+SELECT * 
+FROM world_life_expectancy
+WHERE `Life expectancy` = ''
+;
